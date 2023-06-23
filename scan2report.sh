0b573d7ab9797741b5dd9da2eb79688a504c7fd5
takanori.azegami <takanori.azegami.ta@ultmarc.jp>
2023-06-23 10:13:49 +0900

    cronで実行できるようにシェルを変更

--- scan2report.sh
+++ scan2report.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Configtest
-docker run --rm -it\
+docker run --rm \
     -v ~/.ssh:/root/.ssh:ro \
     -v $PWD:/vuls \
     -v $PWD/vuls-log:/var/log/vuls \
     vuls/vuls configtest -config=./config.toml
 
 # Scan
-docker run --rm -it \
+docker run --rm  \
     -v ~/.ssh:/root/.ssh:ro \
     -v $PWD:/vuls \
     -v $PWD/vuls-log:/var/log/vuls \
@@ -17,9 +17,9 @@ docker run --rm -it \
     vuls/vuls scan -config=./config.toml
 
 # Report
-docker run --rm -it \
+docker run --rm  \
     -v ~/.ssh:/root/.ssh:ro \
     -v $PWD:/vuls \
     -v $PWD/vuls-log:/var/log/vuls \
     -v /etc/localtime:/etc/localtime:ro \
-    vuls/vuls report -format-list -config=./config.toml
+    vuls/vuls report -format-list -ignore-unfixed -cvss-over=7 -config=./config.toml
